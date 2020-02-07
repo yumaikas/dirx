ff4080ad9a680c9aba7d9198c479d3eb5f83e901
yumaikas <yumaikas94@gmail.com>
2020-02-07 01:19:05 -0500

    Fiddling a bit

--- dirx.nim
+++ dirx.nim
@@ -2,9 +2,9 @@ import os, sequtils, strutils, terminal, algorithm
 
 let filtered = filterIt(toSeq(walkDir(".", true)), not(it.path.startsWith(".")))
 let dirs = sortedByIt(filtered, it.path)
-let maxNameLen = max(dirs.mapIt(it.path.len)) + 1
-let totalLen = foldl(dirs.mapIt(it.path.len), a + b + 2)
-var entriesPerLine = max((terminalWidth() - 5) div maxNameLen, 1) - 1
+let maxNameLen = max(dirs.mapIt(it.path.len)) + 2
+let totalLen = foldl(dirs.mapIt(it.path.len), a + b + 2) - 2
+var entriesPerLine = max((terminalWidth() - 5) div maxNameLen, 1)
 var numLines = (dirs.len div entriesPerLine)
 
 if (terminalWidth() - 5) > totalLen:
@@ -16,6 +16,18 @@ proc alignFn(name: string, padLen: int): string =
     return alignLeft(name, padLen)
   return name & "  "
 
+#[
+var colIdx:int = 0
+for entry in dirs:
+  if colIdx == entriesPerLine:
+    colIdx = 0
+  if colIdx == 0:
+    setForegroundColor(fgRed)
+  echo entry.path
+  resetAttributes()
+  inc(colIdx)
+]#
+
 var outputCount = 0
 for entry in dirs:
   if entry.kind == pcFile or entry.kind == pcLinkToFile:
@@ -23,10 +35,10 @@ for entry in dirs:
   if entry.kind == pcDir or entry.kind == pcLinkToDir:
     setForegroundColor(fgBlue)
     stdout.write(entry.path)
-    resetAttributes()
     stdout.write(alignFn("/", maxNameLen - entry.path.len))
+  resetAttributes()
   inc(outputCount)
-  if numLines > 1 and outputCount == entriesPerLine:
+  if numLines > 1 and (outputCount == entriesPerLine):
     stdout.write("\p")
     outputCount = 0
 echo ""
